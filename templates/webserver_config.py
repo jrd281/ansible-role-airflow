b140a92fa40cf6794529295e47839678e028dc37
jrd281 <justindallas@gmail.com>
2024-05-08 22:30:42 -0400

    Committing after changing the auth stuff

--- templates/webserver_config.py
+++ templates/webserver_config.py
@@ -45,7 +45,7 @@ CSRF_ENABLED = True
 # AUTH_LDAP : Is for LDAP
 # AUTH_REMOTE_USER : Is for using REMOTE_USER from web server
 # AUTH_OAUTH : Is for OAuth
-AUTH_TYPE = AUTH_OAUTH
+AUTH_TYPE = AUTH_DB
 
 # Uncomment to setup Full admin role name
 # AUTH_ROLE_ADMIN = 'Admin'
